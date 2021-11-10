ffcbec5c3c38071b9291636d0f2d37120d0aeb92
Alexander Slesarenko <dragongling@gmail.com>
2021-11-10 14:36:13 +0500

    feat: Added team members

--- src/CM.WeeklyTeamReport.Domain/Company.cs
+++ src/CM.WeeklyTeamReport.Domain/Company.cs
@@ -1,8 +1,11 @@
-﻿namespace CM.WeeklyTeamReport.Domain
+﻿using System.Collections.Generic;
+
+namespace CM.WeeklyTeamReport.Domain
 {
     public class Company
     {
         public string Name { get; set; }
+        public ICollection<TeamMember> TeamMembers { get; set; }
 
         public Company(string name)
         {
