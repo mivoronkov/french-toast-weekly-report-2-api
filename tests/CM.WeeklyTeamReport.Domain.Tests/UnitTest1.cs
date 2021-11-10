beb09a50306c952e17a682ce5450464f03b2824f
Alexander Slesarenko <dragongling@gmail.com>
2021-11-10 09:37:32 +0500

    test: Added tests project for domain class library

---
+++ tests/CM.WeeklyTeamReport.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
