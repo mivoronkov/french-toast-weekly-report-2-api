f6a66026fc1f171477e1c5521fbfe3a584f8ec78
Alexander Slesarenko <dragongling@gmail.com>
2021-11-20 16:30:19 +0500

    feat: added DB project

---
+++ src/CM.WeeklyTeamReport.DB/dbo/Tables/Company.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Company] (
+    [CompanyId]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50) NOT NULL,
+    [CreationDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([CompanyId] ASC)
+);
+
