a0e30c8462ed933ce8cbcda49c02753bdc8f5051
Alexander Slesarenko <dragongling@gmail.com>
2021-11-23 10:15:09 +0500

    fix: restored missing unit tests

---
+++ tests/CM.WeeklyTeamReport.Domain.Tests/Entities/CompanyTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class CompanyTests
+    {
+        [Fact]
+        public void ShouldCreateCompany()
+        {
+            string companyName = "CompanyName";
+            var creationDate = DateTime.Now;
+            var company = new Company { 
+                Name = companyName,
+                CreationDate = creationDate
+            };
+            Assert.Equal(companyName, company.Name);
+            Assert.Equal(creationDate, company.CreationDate);
+        }
+
+        [Fact]
+        public void ShouldAllowToSetName()
+        {
+            string companyName = "CompanyName";
+            var company = new Company { Name = companyName };
+            Assert.Equal(companyName, company.Name);
+
+            companyName = "AnotherCompanyName";
+            company.Name = companyName;
+            Assert.Equal(companyName, company.Name);
+        }
+    }
+}
---
+++ tests/CM.WeeklyTeamReport.Domain.Tests/Entities/TeamMemberTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class TeamMemberTests
+    {
+        [Fact]
+        public void ShouldCreateTeamMember()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember { 
+                FirstName = "FirstName", 
+                LastName = "LastName", 
+                Title = "Title", 
+                Email = email 
+            };
+            Assert.Equal("FirstName", tm.FirstName);
+            Assert.Equal("LastName", tm.LastName);
+            Assert.Equal("Title", tm.Title);
+            Assert.Equal("mail@example.com", tm.Email.Address);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToChangeFirstName()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+            tm.FirstName = "NewFirstName";
+            Assert.Equal("NewFirstName", tm.FirstName);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToChangeLastName()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+            tm.LastName = "NewLastName";
+            Assert.Equal("NewLastName", tm.LastName);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToChangeTitle()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+            tm.Title = "NewTitle";
+            Assert.Equal("NewTitle", tm.Title);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToChangeEmail()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+            var newEmail = new MailAddress("new_mail@example.com");
+            tm.Email = newEmail;
+            Assert.Equal(newEmail, tm.Email);
+        }
+
+
+        [Fact]
+        public void ShouldGenerateInviteLink()
+        {
+            var email = new MailAddress("mail@example.com");
+            var tm = new TeamMember
+            {
+                ID = 1,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+
+            // Invite link is a link
+            Assert.True(Uri.IsWellFormedUriString(tm.InviteLink, UriKind.Absolute));
+
+            // Invite link hasn't set method
+            Assert.Null(typeof(TeamMember)?.GetProperty(nameof(TeamMember.InviteLink))?.GetSetMethod());
+
+            // Invite link is unique for each ID
+            var tm2 = new TeamMember
+            {
+                ID = 2,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                Email = email
+            };
+            Assert.NotEqual(tm2.InviteLink, tm.InviteLink);
+
+            // TODO: check if server responds to invite urls ASAP
+        }
+    }
+}
