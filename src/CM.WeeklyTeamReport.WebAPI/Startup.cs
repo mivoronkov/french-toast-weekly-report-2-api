0d9cff3ae7b0fc20f45c0dd985d8fd128f374daf
Alexander Slesarenko <dragongling@gmail.com>
2021-11-24 01:22:59 +0500

    test: suppressed unnecessary coverage

--- src/CM.WeeklyTeamReport.WebAPI/Startup.cs
+++ src/CM.WeeklyTeamReport.WebAPI/Startup.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//ncrunch: no coverage start
 namespace CM.WeeklyTeamReport.WebAPI
 {
     public class Startup
