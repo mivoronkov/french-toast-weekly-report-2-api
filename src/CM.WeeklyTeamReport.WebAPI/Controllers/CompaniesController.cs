a0a02d35aee97ea208301aa6b9c2ee04f508394a
Alexander Slesarenko <dragongling@gmail.com>
2021-11-24 01:03:51 +0500

    feat: added companies controller

---
+++ src/CM.WeeklyTeamReport.WebAPI/Controllers/CompaniesController.cs
@@ -0,0 +1,29 @@
+﻿using CM.WeeklyTeamReport.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CM.WeeklyTeamReport.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : ControllerBase
+    {
+        private IRepository<Company> _repository;
+
+        public CompaniesController(IRepository<Company> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: CompaniesController
+        [HttpGet]
+        public ICollection<Company> Get()
+        {
+            return _repository.ReadAll();
+        }
+    }
+}
---
+++ tests/CM.WeeklyTeamReport.WebAPI.Tests/Controllers/CompaniesControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using CM.WeeklyTeamReport.Domain;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.WebAPI.Controllers.Tests
+{
+    public class CompaniesControllerTests
+    {
+        [Fact]
+        public void ShouldReturnAllCompanies()
+        {
+            var fixture = new CompaniesControllerFixture();
+            fixture.CompanyRepository
+                .Setup(x => x.ReadAll())
+                .Returns(new List<Company>() {
+                    new Company { Name = "Trevor Philips Industries" },
+                    new Company { Name = "Aperture Science" }
+                });
+            var controller = fixture.GetCompaniesController();
+            var companies = controller.Get();
+
+            companies.Should().NotBeNull();
+            companies.Should().HaveCount(2);
+
+            fixture
+                .CompanyRepository
+                .Verify(x => x.ReadAll(), Times.Once);
+        }
+
+        public class CompaniesControllerFixture
+        {
+            public CompaniesControllerFixture()
+            {
+                CompanyRepository = new Mock<IRepository<Company>>();
+            }
+
+            public Mock<IRepository<Company>> CompanyRepository { get; private set; }
+
+            public CompaniesController GetCompaniesController()
+            {
+                return new CompaniesController(CompanyRepository.Object);
+            }
+        }
+    }
+}
