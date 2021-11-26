6eeff44dab1d1b7c1a32edb4ace02b6803628a9a
Alexander Slesarenko <dragongling@gmail.com>
2021-11-26 16:08:04 +0500

    refactoring: composition instead of inheritance for handling complex API routes

--- src/CM.WeeklyTeamReport.WebAPI/Controllers/MembersController.cs
+++ src/CM.WeeklyTeamReport.WebAPI/Controllers/MembersController.cs
@@ -1,4 +1,5 @@
 ﻿using CM.WeeklyTeamReport.Domain;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,55 @@ using System.Threading.Tasks;
 namespace CM.WeeklyTeamReport.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class MembersController : RESTController<TeamMember>
+    [Route("api/Companies/{companyId}/Members")]
+    public class MembersController
     {
-        protected override string EntityEndpoint => "Members";
+        private readonly MembersRESTController rest;
 
-        public MembersController(IRepository<TeamMember> repo) : base(repo) { }
+        public MembersController(IRepository<TeamMember> repo) {
+            rest = new MembersRESTController(repo);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return rest.Get();
+        }
+
+        [HttpGet]
+        [Route("{memberId}")]
+        public IActionResult GetSingle(int companyId, int memberId)
+        {
+            return rest.GetSingle(memberId);
+        }
+
+        [HttpPost]
+        public IActionResult Create(int companyId, TeamMember entity)
+        {
+            entity.CompanyId = companyId;
+            return rest.Create(entity);
+        }
+
+        [HttpPut]
+        [Route("{memberId}")]
+        public IActionResult Put(int companyId, int memberId, TeamMember entity)
+        {
+            entity.CompanyId = companyId;
+            return rest.Put(memberId, entity);
+        }
+
+        [HttpDelete]
+        [Route("{memberId}")]
+        public IActionResult Delete(int companyId, int memberId)
+        {
+            return rest.Delete(memberId);
+        }
+
+        private class MembersRESTController : RESTController<TeamMember>
+        {
+            public override string EntitiesEndpoint => "api/Companies/{companyId}/Members";
+
+            public MembersRESTController(IRepository<TeamMember> repo) : base(repo) { }
+        }
     }
 }
--- src/CM.WeeklyTeamReport.WebAPI/Controllers/ReportsController.cs
+++ src/CM.WeeklyTeamReport.WebAPI/Controllers/ReportsController.cs
@@ -8,11 +8,53 @@ using System.Threading.Tasks;
 namespace CM.WeeklyTeamReport.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class ReportsController : RESTController<WeeklyReport>
+    [Route("api/Companies/{companyId}/Members/{memberId}/Reports")]
+    public class ReportsController
     {
-        protected override string EntityEndpoint => "Reports";
+        private readonly ReportsRESTController rest;
 
-        public ReportsController(IRepository<WeeklyReport> repo) : base(repo) { }
+        public ReportsController(IRepository<WeeklyReport> repo){
+            rest = new ReportsRESTController(repo);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return rest.Get();
+        }
+
+        [HttpGet]
+        [Route("{reportId}")]
+        public IActionResult GetSingle(int memberId)
+        {
+            return rest.GetSingle(memberId);
+        }
+
+        [HttpPost]
+        public IActionResult Create(WeeklyReport entity)
+        {
+            return rest.Create(entity);
+        }
+
+        [HttpPut]
+        [Route("{reportId}")]
+        public IActionResult Put(int reportId, WeeklyReport entity)
+        {
+            return rest.Put(reportId, entity);
+        }
+
+        [HttpDelete]
+        [Route("{reportId}")]
+        public IActionResult Delete(int reportId)
+        {
+            return rest.Delete(reportId);
+        }
+
+        private class ReportsRESTController : RESTController<WeeklyReport>
+        {
+            public override string EntitiesEndpoint => "api/Companies/{companyId}/Members/{memberId}/Reports";
+
+            public ReportsRESTController(IRepository<WeeklyReport> repo) : base(repo) { }
+        }
     }
 }
