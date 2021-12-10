9c0d471cad65d9a769b6ba061010b5f25ba63b3a
Alexander Slesarenko <dragongling@gmail.com>
2021-12-10 17:22:48 +0500

    feat: added date in WeeklyReport table

--- src/CM.WeeklyTeamReport.DB/dbo/Tables/WeeklyReport.sql
+++ src/CM.WeeklyTeamReport.DB/dbo/Tables/WeeklyReport.sql
@@ -7,10 +7,13 @@
     [HighThisWeek]    NVARCHAR (600) NOT NULL,
     [LowThisWeek]     NVARCHAR (600) NOT NULL,
     [AnythingElse]    NVARCHAR (600) NULL,
+    [Date]            DATE           NOT NULL,
     PRIMARY KEY CLUSTERED ([ReportId] ASC),
     CONSTRAINT [FK_Report_Author] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[TeamMember] ([TeamMemberId]),
     CONSTRAINT [FK_Report_MoraleGrade] FOREIGN KEY ([MoraleGradeId]) REFERENCES [dbo].[ReportGrade] ([ReportGradeId]),
     CONSTRAINT [FK_Report_StressGrade] FOREIGN KEY ([StressGradeId]) REFERENCES [dbo].[ReportGrade] ([ReportGradeId]),
     CONSTRAINT [FK_Report_WorkloadGrade] FOREIGN KEY ([WorkloadGradeId]) REFERENCES [dbo].[ReportGrade] ([ReportGradeId])
 );
+
+
 
