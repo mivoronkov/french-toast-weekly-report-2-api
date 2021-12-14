bc42c14d9c9fd3fd1d80367b92c6580c8cf8a888
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-14 13:03:29 +0300

    fix: integration tests fix

--- tests/CM.WeeklyTeamReport.Domain.IntegrationTests/Repositories/CompanyRepositoryTests.cs
+++ tests/CM.WeeklyTeamReport.Domain.IntegrationTests/Repositories/CompanyRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using CM.WeeklyTeamReport.Domain.Entities.Interfaces;
+using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.IO;
@@ -34,45 +35,6 @@ namespace CM.WeeklyTeamReport.Domain.IntegrationTests
             var repository = new CompanyRepository(SetupTests.Configuration);
             var result = repository.ReadAll();
             result.Should().AllBeOfType<Company>();
-        }
-
-        [Fact]
-        public void ShouldReturnEmployees()
-        {
-            var companyRepo = new CompanyRepository(SetupTests.Configuration);
-            var company = companyRepo.Create(new Company { Name = "Test company" });
-            var tmRepo = new TeamMemberRepository(SetupTests.Configuration);
-            var tms = new List<TeamMember>(){
-                tmRepo.Create(new TeamMember {
-                    FirstName = "F1",
-                    LastName = "L1",
-                    Title = "T1",
-                    Email = "m1@mail.com",
-                    CompanyId = company.ID
-                }),
-                tmRepo.Create(new TeamMember
-                {
-                    FirstName = "F2",
-                    LastName = "L2",
-                    Title = "T2",
-                    Email = "m2@mail.com",
-                    CompanyId = company.ID
-                }),
-                tmRepo.Create(new TeamMember
-                {
-                    FirstName = "F3",
-                    LastName = "L3",
-                    Title = "T3",
-                    Email = "m3@mail.com",
-                    CompanyId = company.ID
-                })
-            };
-            companyRepo.GetTeamMembers(company).Should().BeEquivalentTo(tms);
-            foreach (var tm in tms)
-            {
-                tmRepo.Delete(tm);
-            }
-            companyRepo.Delete(company);
         }
     }
 }
