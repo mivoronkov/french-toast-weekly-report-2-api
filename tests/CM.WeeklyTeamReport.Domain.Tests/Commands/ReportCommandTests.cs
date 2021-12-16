ed99c89412c289071d85ca2dc6069e87e7aec22d
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-16 18:01:44 +0300

    feat: fullReportToDto command test

--- tests/CM.WeeklyTeamReport.Domain.Tests/Commands/ReportCommandTests.cs
+++ tests/CM.WeeklyTeamReport.Domain.Tests/Commands/ReportCommandTests.cs
@@ -96,5 +96,49 @@ namespace CM.WeeklyTeamReport.Domain.Tests
             Assert.Equal(reportDto.Date, report.Date);
             Assert.Equal(reportDto.AnythingElse, report.AnythingElse);
         }
+        [Fact]
+        public void ShouldCreateDtoFromFullReport()
+        {
+            const string HighThisWeek = "My high this week";
+            const string LowThisWeek = "My low this week";
+            var reportDate = new DateTime(2021, 11, 10);
+            var anythingElse = "Anything else";
+            var commentary = "nope";
+
+            var report = new FullWeeklyReport
+            {
+                ID = 1,
+                AuthorId = 1,
+                MoraleGradeId = 1,
+                StressGradeId = 1,
+                WorkloadGradeId = 1,
+                MoraleLevel = 1,
+                StressLevel = 1,
+                WorkloadLevel = 1,
+                MoraleCommentary = commentary,
+                StressCommentary = commentary,
+                WorkloadCommentary = commentary,
+                HighThisWeek = HighThisWeek,
+                LowThisWeek = LowThisWeek,
+                AnythingElse = anythingElse,
+                Date = reportDate
+            };
+            var reportDto = commands.fullReportToDto(report);
+            Assert.Equal(reportDto.ID, report.ID);
+            Assert.Equal(reportDto.AuthorId, report.AuthorId);
+            Assert.Equal(reportDto.MoraleGradeId, report.MoraleGradeId);
+            Assert.Equal(reportDto.StressGradeId, report.StressGradeId);
+            Assert.Equal(reportDto.WorkloadGradeId, report.WorkloadGradeId);
+            Assert.Equal(reportDto.MoraleGrade.Commentary, report.MoraleCommentary);
+            Assert.Equal(reportDto.StressGrade.Commentary, report.StressCommentary);
+            Assert.Equal(reportDto.WorkloadGrade.Commentary, report.WorkloadCommentary);
+            Assert.Equal(reportDto.MoraleGrade.Level, (Level)report.MoraleLevel);
+            Assert.Equal(reportDto.StressGrade.Level, (Level)report.StressLevel);
+            Assert.Equal(reportDto.WorkloadGrade.Level, (Level)report.WorkloadLevel);
+            Assert.Equal(reportDto.HighThisWeek, report.HighThisWeek);
+            Assert.Equal(reportDto.LowThisWeek, report.LowThisWeek);
+            Assert.Equal(reportDto.Date, report.Date);
+            Assert.Equal(reportDto.AnythingElse, report.AnythingElse);
+        }
     }
 }
