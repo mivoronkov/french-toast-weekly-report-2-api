bf5d9b321fd89e3327b80ab96226a829ed727603
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-17 15:07:32 +0300

    fix [db]: changed PK

--- src/CM.WeeklyTeamReport.DB/dbo/Tables/ReportingTeamMemberToTeamMember.sql
+++ src/CM.WeeklyTeamReport.DB/dbo/Tables/ReportingTeamMemberToTeamMember.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[ReportingTeamMemberToTeamMember] (
-    [LinkId] INT IDENTITY (1, 1) NOT NULL,
     [ReportingTMId] INT NOT NULL,
     [LeaderTMId]    INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([LinkId] ASC),
+    CONSTRAINT [PK_TableName] PRIMARY KEY CLUSTERED ([ReportingTMId], [LeaderTMId]),
     CONSTRAINT [FK_LeaderTM] FOREIGN KEY ([LeaderTMId]) REFERENCES [dbo].[TeamMember] ([TeamMemberId]),
     CONSTRAINT [FK_ReportingTM] FOREIGN KEY ([ReportingTMId]) REFERENCES [dbo].[TeamMember] ([TeamMemberId])
 );
