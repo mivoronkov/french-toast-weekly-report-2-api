8af326dfbe8e9c94d5a988c66014c94b5ac6c7fd
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-17 14:43:47 +0300

    feat: team link repository

--- src/CM.WeeklyTeamReport.DB/dbo/Tables/ReportingTeamMemberToTeamMember.sql
+++ src/CM.WeeklyTeamReport.DB/dbo/Tables/ReportingTeamMemberToTeamMember.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[ReportingTeamMemberToTeamMember] (
+    [LinkId] INT IDENTITY (1, 1) NOT NULL,
     [ReportingTMId] INT NOT NULL,
     [LeaderTMId]    INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([LinkId] ASC),
     CONSTRAINT [FK_LeaderTM] FOREIGN KEY ([LeaderTMId]) REFERENCES [dbo].[TeamMember] ([TeamMemberId]),
     CONSTRAINT [FK_ReportingTM] FOREIGN KEY ([ReportingTMId]) REFERENCES [dbo].[TeamMember] ([TeamMemberId])
 );
