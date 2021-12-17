83c362cbc389ca0e79b88520f58f6579380526fb
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-17 14:16:46 +0300

    feat: teamLink entity

---
+++ tests/CM.WeeklyTeamReport.Domain.Tests/Entities/TeamLinkTests.cs
@@ -0,0 +1,22 @@
+﻿using CM.WeeklyTeamReport.Domain.Entities.Implementations;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class TeamLinkTests
+    {
+        [Fact]
+        public void ShouldCreateTeamLink()
+        {
+            var teamLink = new TeamLink
+            {
+                ReportingTMId = 1,
+                LeaderTMId = 2,
+            };
+            Assert.Equal(1, teamLink.ReportingTMId);
+            Assert.Equal(2, teamLink.LeaderTMId);
+        }
+    }
+}
