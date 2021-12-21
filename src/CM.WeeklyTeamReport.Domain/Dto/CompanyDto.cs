0310c4cd1783eed17423b66563e98d8027d20e5c
Alexander Slesarenko <dragongling@gmail.com>
2021-12-21 14:21:29 +0500

    feat: company and team member validation

--- src/CM.WeeklyTeamReport.Domain/Dto/CompanyDto.cs
+++ src/CM.WeeklyTeamReport.Domain/Dto/CompanyDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace CM.WeeklyTeamReport.Domain.Repositories.Dto
     public class CompanyDto
     {
         public int? ID { get; set; } = 0;
+
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
         public DateTime? CreationDate { get; set; }
         public CompanyDto() { }
--- src/CM.WeeklyTeamReport.Domain/Dto/TeamMemberDto.cs
+++ src/CM.WeeklyTeamReport.Domain/Dto/TeamMemberDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,17 @@ namespace CM.WeeklyTeamReport.Domain.Repositories.Dto
     public class TeamMemberDto
     {
         public int? ID { get; set; }
+
+        [Required]
+        [MinLength(2)]
         public string FirstName { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string LastName { get; set; }
 
+        [Required]
+        [MinLength(2)]
         public string Title { get; set; }
 
         public string Email { get; set; }
