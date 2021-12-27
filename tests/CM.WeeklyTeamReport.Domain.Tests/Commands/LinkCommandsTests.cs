b77dfec8748197f6a7e6df1cd48ddc98b72faa6f
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-27 19:06:35 +0300

    feat: link command test

---
+++ tests/CM.WeeklyTeamReport.Domain.Tests/Commands/LinkCommandsTests.cs
@@ -0,0 +1,28 @@
+﻿using CM.WeeklyTeamReport.Domain.Commands;
+using CM.WeeklyTeamReport.Domain.Repositories.Dto;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class LinkCommandsTests
+    {
+        public LinkCommands commands = new LinkCommands();
+        [Fact]
+        public void ShouldBeDifference()
+        {
+            int[] expected = new[] { 4, 5 };
+            int[] substr = new[] { 1, 2 };
+            int[] source = new[] { 1, 2, 4, 5 };
+
+
+            var result = commands.LinksDifference(substr, source);
+            var en = result.GetEnumerator();
+            en.MoveNext();
+            Assert.Equal(expected[0], en.Current);
+            en.MoveNext();
+            Assert.Equal(expected[1], en.Current);
+        }
+    }
+}
